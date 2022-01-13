ad94e308eaf3597fee5e653c579d73a1326ca9ec
Samy Cheklat <samycheklat@hotmail.fr>
2022-01-12 23:20:54 -0500

    looking if user exists before adding new user

--- server/app.js
+++ server/app.js
@@ -1,45 +1,72 @@
 const express = require("express");
-const { connectDb, getProvinces, insertUser } = require("./connect.js");
+const {
+  connectDb,
+  getProvinces,
+  insertUser,
+  fetchDBData,
+  findUser,
+  validateUser,
+  verifyExistingUser,
+} = require("./connect.js");
 const cors = require("cors");
-let { people } = require("./data.js");
 const app = express();
 const port = 5000;
 const corsOption = {
   origin: "http://localhost:8080",
   optionsSuccessStatus: 200,
-  preflightContinue: false,
 };
 
 app.use(express.json());
-app.options("/api/provinces", cors(corsOption));
-app.options("/api/users", cors(corsOption));
+app.options("*", cors(corsOption));
 
-app.get("/api/provinces", cors(corsOption), async (req, res) => {
+app.get("/api/provinces", cors(), async (req, res) => {
   let response = await getProvinces();
 
   if (response.statusCode === 404) {
     return res.status(404).send({ success: false, msg: "An error has occured, nothing was found" });
   }
-
   return res.status(200).json({ success: true, data: response });
 });
 
-app.post("/api/users", cors(corsOption), async (req, res) => {
+app.post("/api/login", cors(), async (req, res) => {
+  let user = { username: req.body.username, password: req.body.password };
+  const response = await fetchDBData();
+  const statusCode = Number(response.statusCode);
+  const userCredentials = await findUser(user.username);
+  console.log(user.password);
+  console.log(userCredentials.data.username);
+  const authorization = validateUser(userCredentials.data.username, user.password);
+  console.log(authorization);
+  if (statusCode !== 201) {
+    return res.status(401).send("A connection error has occured please try again later");
+  }
+
+  return res.status(201).json({ success: true, msg: "Request successful", data: response.data });
+});
+
+app.post("/api/users", cors(), async (req, res) => {
   let user = {
-    email: req.body.userEmail,
+    username: req.body.userName,
     password: req.body.userPassword,
+    email: req.body.userEmail,
     address: req.body.userAddress,
     city: req.body.userCity,
     province: req.body.userProvince,
     zip: req.body.userZip,
   };
-  const response = await insertUser(user);
-  statusCode = Number(response.statusCode);
-  if (statusCode !== 201) {
-    return res.status(401).send("A connection error has occured please try again later");
+  const verifyingExistingUser = await verifyExistingUser(user.username);
+  if (verifyingExistingUser.statusCode === 409) {
+    return res.status(409).json({ success: false, code: 409, data: "", msg: "User already exists" });
+  } else {
+    const response = await insertUser(user);
+    const statusCode = Number(response.statusCode);
+    if (statusCode !== 201) {
+      return res.status(401).send("A connection error has occured please try again later");
+    }
+    const date = new Date().toLocaleString();
+    console.log(` The "${req.method}" request was successful!\n Request status code: ${statusCode}\n Time: ${date} `);
+    return res.status(201).json({ success: true, data: user, msg: "Request succesful" });
   }
-  console.log(` The "${req.method}" request was successful!\n Request status code: ${statusCode} `);
-  return res.status(201).json({ success: true, data: user, msg: "Request succesful" });
 });
 
 const start = async () => {
--- server/connect.js
+++ server/connect.js
@@ -1,4 +1,4 @@
-const { MongoClient, Db } = require("mongodb");
+const { MongoClient } = require("mongodb");
 const env = require("dotenv");
 env.config();
 const uri = process.env.MONGODB_URI;
@@ -7,7 +7,6 @@ const connectDb = async () => {
   try {
     const client = await MongoClient.connect(uri);
     const database = client.db("CashAndCarryCrypto");
-    const collection = await database.collection("provinces").find().toArray();
     console.log(`Connected to the DB...`);
   } catch (error) {
     console.log(error);
@@ -20,8 +19,9 @@ const insertUser = async (user) => {
     const client = await MongoClient.connect(uri);
     const database = client.db("CashAndCarryCrypto");
     const data = await database.collection("users").insertOne({
-      email: user.email,
+      username: user.username,
       password: user.password,
+      email: user.email,
       address: user.address,
       city: user.city,
       province: user.province,
@@ -33,6 +33,23 @@ const insertUser = async (user) => {
   }
   return response;
 };
+
+const fetchDBData = async () => {
+  let response = { statusCode: "", data: "" };
+  try {
+    const client = await MongoClient.connect(uri);
+    const database = client.db("CashAndCarryCrypto");
+    const collection = await database.collection("users").find().toArray();
+    response = {
+      statusCode: 201,
+      data: collection,
+    };
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const getProvinces = async () => {
   let response = { statusCode: "", data: "" };
   try {
@@ -51,4 +68,58 @@ const getProvinces = async () => {
   return response;
 };
 
-module.exports = { connectDb, getProvinces, insertUser };
+const findUser = async (username) => {
+  let response = { statusCode: "", data: "", msg: "" };
+  try {
+    const client = await MongoClient.connect(uri);
+    const database = client.db("CashAndCarryCrypto");
+    const user = await database.collection("users").findOne({ username: username });
+    if (user === null) throw "User not found";
+    return (response = {
+      statusCode: 200,
+      data: user,
+      msg: "User fetched successfully",
+    });
+  } catch (error) {
+    return (response = {
+      statusCode: 404,
+      data: "",
+      msg: "User not found",
+    });
+  }
+};
+
+const validateUser = (userPassword, reqPassword) => {
+  let response = { statusCode: "", data: "", msg: "" };
+  console.log(`connect userpassword: ${userPassword}, connect reqpassword: ${reqPassword}`);
+  if (userPassword === reqPassword) {
+    return (response = {
+      statusCode: 202,
+      data: "",
+      msg: "Authorized",
+    });
+  }
+  return (response = {
+    statusCode: 401,
+    data: "",
+    msg: "Unauthorized",
+  });
+};
+
+const verifyExistingUser = async (username) => {
+  let response = { statusCode: "", data: "", msg: "" };
+  try {
+    const client = await MongoClient.connect(uri);
+    const database = client.db("CashAndCarryCrypto");
+    const existingUser = await database.collection("users").findOne({ username: username });
+    if (existingUser !== null) {
+      response = { statusCode: 409, data: "", msg: "User already exist" };
+      return response;
+    }
+    response = { statusCode: 201, data: "", msg: "User doesn't exist" };
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+module.exports = { connectDb, getProvinces, insertUser, fetchDBData, findUser, validateUser, verifyExistingUser };
